382f38c9395a1de051cfc9ec22f05519f3e9e6e3
Alket Tarko <alkettarko@outlook.com>
2020-12-18 10:37:32 +0100

    first commit

---
+++ src/test/java/com/springbootplayground/SpringBootPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
